e0307f9656168fdb1176add625f85c1bd18384a1
David McGrann <dpmcgrann@gmail.com>
2018-07-17 14:11:28 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,3 +1,13 @@
 class CreateShows < ActiveRecord::Migration
   
+  def change
+    create_table :shows do |t|
+      t.text :name
+      t.text :network
+      t.text :day 
+      t.string :image_url
+      t.timestamps
+    end
+  end
+  
 end
