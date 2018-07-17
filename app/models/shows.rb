2d14f5a503f1d53b5753ddc3d12dde9f6816273d
David McGrann <dpmcgrann@gmail.com>
2018-07-17 14:39:33 +0000

    automatically backed up by learn

--- app/models/shows.rb
+++ app/models/shows.rb
@@ -5,7 +5,8 @@ class Show < ActiveRecord::Base
   end
   
   def self.most_popular_show
-    Show.limit(1).order(rating: :desc)
+    show = Show.limit(1).order(rating: :desc)
+    show
   end
   
 end
